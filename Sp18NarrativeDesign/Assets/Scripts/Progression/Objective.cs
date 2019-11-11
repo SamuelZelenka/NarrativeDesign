d502797c3a582002a723ab4950ffdfcf1724260b
peanutpowah <samuel.zelenka@hotmail.com>
2019-11-11 15:27:59 +0100

    Added Progression scripts

---
+++ Sp18NarrativeDesign/Assets/Scripts/Progression/Objective.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objective : Interactible
+{
+    string objectiveTitle;
+    public override void Interact()
+    {
+        
+    }
+
+}
---
+++ Sp18NarrativeDesign/Assets/Scripts/Progression/ProgressionTracker.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressionTracker : MonoBehaviour
+{
+    List<Objective> CompletedObjectives;
+
+    void Add
+}
