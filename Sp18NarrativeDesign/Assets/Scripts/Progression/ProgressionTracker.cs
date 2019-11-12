304e33be4f37eb66ea5462e8b75b94bda545dd44
peanutpowah <samuel.zelenka@hotmail.com>
2019-11-12 13:41:31 +0100

    static classes

--- Sp18NarrativeDesign/Assets/Scripts/Progression/ProgressionTracker.cs
+++ Sp18NarrativeDesign/Assets/Scripts/Progression/ProgressionTracker.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class ProgressionTracker
+public class ProgressionTracker
 {
     public static List<Objective> completedObjectives = new List<Objective>();
 
