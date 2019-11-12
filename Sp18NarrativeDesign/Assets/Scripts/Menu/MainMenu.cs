ac297afa9233ab1f960f535cea80d06c04b47145
Anteey <57527040+Anteey@users.noreply.github.com>
2019-11-12 11:20:13 +0100

    Lagt till huvudmeny med play, options, credits och exit med fungerande funktioner

---
+++ Sp18NarrativeDesign/Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Add options and credits window and respetive close buttons
+    [SerializeField]
+    private GameObject optionsMenu;
+    [SerializeField]
+    private GameObject creditsWindow;
+    //
+    //These open and close optios and credits windows
+    //
+    public void OptionsButtonPressed(){
+        if (!optionsMenu.activeSelf){
+            optionsMenu.SetActive(true);
+        }
+        else{
+            optionsMenu.SetActive(false);
+        } 
+    }
+    public void OptionsCloseButtonPressed(){
+        optionsMenu.SetActive(false);
+    }
+    public void CreditsButtonPressed(){
+        if (!creditsWindow.activeSelf){
+            creditsWindow.SetActive(true);
+        }
+        else{
+            creditsWindow.SetActive(false);
+        } 
+    }
+    public void CreditsCloseButtonPressed(){
+        creditsWindow.SetActive(false);
+    }
+    public void PlayButtonPressed()
+	{
+		SceneManager.LoadScene("MainScene");
+	}
+    public void ExitGame(){
+        Application.Quit();
+    }
+}
