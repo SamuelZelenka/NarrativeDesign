85560dabda47f445ea421e7afb5cd5a146f874f1
Mohamed <mohamed.longis.hassan@gmail.com>
2019-11-14 12:08:39 +0100

    PickUp
    Pickup items..

---
+++ Sp18NarrativeDesign/Assets/Scripts/PickUp.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PickUp : MonoBehaviour
+{
+
+    public GameObject tempParent;
+    private GameObject item;
+    public bool isHolding = false;
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log(other.transform.tag);
+        if (other.transform.tag == "Moveable" && Input.GetKey(KeyCode.F) && !isHolding)
+        {
+            isHolding = true;
+            item = other.gameObject;
+            item.transform.parent = tempParent.transform;
+            item.transform.position = item.transform.parent.position + new Vector3(0,0.5f,0) + item.transform.parent.forward * 0.5f;
+            item.GetComponent<Rigidbody>().useGravity = false;
+            item.GetComponent<Rigidbody>().detectCollisions = true;
+            item.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            item.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        }
+        else
+        {
+            if (item != null)
+            {
+                isHolding = false;
+                item.transform.parent = null;
+                item.GetComponent<Rigidbody>().useGravity = true;
+                item = null;
+
+            }
+        }
+
+    }
+
+}
+
