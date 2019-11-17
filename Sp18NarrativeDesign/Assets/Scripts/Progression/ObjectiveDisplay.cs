344040de4c9b8064c01a6f59838b9c1f3e13e981
peanutpowah <samuel.zelenka@hotmail.com>
2019-11-17 18:32:28 +0100

    Rework objectives.
    - Custom Event system for objectives
    - Added Visual log in-game
    - Editor script for easy implementation of objectives

---
+++ Sp18NarrativeDesign/Assets/Scripts/Progression/ObjectiveDisplay.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectiveDisplay : MonoBehaviour
+{
+    Text textLog;
+    IEnumerator coroutine;
+    [SerializeField] Queue<string> textQueue = new Queue<string>();
+
+    public bool coroutineRunning;
+
+    private void Start()
+    {
+        textLog = GetComponent<Text>();
+        ObjectiveManager.objectiveManager.onObjectiveInteractAdd += AddText;
+        ObjectiveManager.objectiveManager.objectiveComplete += AddText;
+    }
+    private void Update()
+    {
+        if (textQueue.Count != 0 && !coroutineRunning)
+        {
+            coroutine = UpdateLog(2);
+            StartCoroutine(coroutine);
+        }
+    }
+
+
+    void AddText()
+    {
+        textQueue.Enqueue("Objective Completed.");
+    }
+
+    void AddText(Objective newObjective)
+    {
+        textQueue.Enqueue(newObjective.objectiveText);
+    }
+
+
+    IEnumerator UpdateLog(float waitTime)
+    {
+        coroutineRunning = true;
+        textLog.text = textQueue.Dequeue();
+
+        for (float i = waitTime; i > -0.9f; i -= 0.01f)
+        {
+
+            Color newColor = textLog.color;
+            newColor.a = i;
+            textLog.color = newColor;
+            yield return new WaitForSeconds(.01f);
+        }
+        coroutineRunning = false;
+    }
+}
+
+
---
+++ Sp18NarrativeDesign/Assets/Scripts/Progression/ObjectiveInteractable.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class ObjectiveInteractable : Interactible
+{
+    public enum ObjectiveType { Start, End, Both}
+
+    public ObjectiveType myType;
+    [HideInInspector]
+    public string startTitle;
+    [HideInInspector]
+    public string endTitle;
+    [HideInInspector]
+    public string objectiveText;
+
+    Objective objective;
+
+    public override void Interact()
+    {
+        objective = new Objective(startTitle, objectiveText);
+        switch (myType)
+        {
+            case ObjectiveType.Start:
+                ObjectiveManager.objectiveManager.ObjectiveInteractAdd(objective);
+                break;
+            case ObjectiveType.End:
+                ObjectiveManager.objectiveManager.ObjectiveInteractComplete(endTitle);
+                break;
+            case ObjectiveType.Both:
+                ObjectiveManager.objectiveManager.ObjectiveInteractComplete(endTitle);
+                ObjectiveManager.objectiveManager.ObjectiveInteractAdd(objective);
+                break;
+            default:
+                break;
+        }
+
+        
+        ObjectiveManager.UpdateObjectiveLog();
+        Destroy(this.gameObject);
+    }
+
+}
+
+#if UNITY_EDITOR
+
+[CustomEditor(typeof(ObjectiveInteractable))]
+public class TestCustomInspector : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        ObjectiveInteractable script = (ObjectiveInteractable)target;
+
+        script.myType = (ObjectiveInteractable.ObjectiveType)EditorGUILayout.EnumPopup("My type", script.myType);
+
+
+        switch (script.myType)
+        {
+            case ObjectiveInteractable.ObjectiveType.Start:
+                script.startTitle = EditorGUILayout.TextField("Objective Title", script.startTitle);
+                script.objectiveText = EditorGUILayout.TextField("Objective Text", script.objectiveText);
+                break;
+            case ObjectiveInteractable.ObjectiveType.End:
+                script.endTitle = EditorGUILayout.TextField("Complete Title", script.endTitle);
+                break;
+            case ObjectiveInteractable.ObjectiveType.Both:
+                script.startTitle = EditorGUILayout.TextField("Objective Title", script.startTitle);
+                script.objectiveText = EditorGUILayout.TextField("Objective Text", script.objectiveText);
+                script.endTitle = EditorGUILayout.TextField("Complete Title", script.endTitle);
+                break;
+            default:
+                break;
+        }
+    }
+}
+#endif
