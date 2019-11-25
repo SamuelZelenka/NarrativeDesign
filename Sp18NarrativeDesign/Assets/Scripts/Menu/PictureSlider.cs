437ff8748ff94f6548fa4f27df99d263e49cd8e2
undead-moose <undead-moose@users.noreply.github.com>
2019-11-25 11:28:31 +0100

    slideshow
    slideshow script är klart men scen behöver göras

---
+++ Sp18NarrativeDesign/Assets/Scripts/Menu/PictureSlider.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PictureSlider : MonoBehaviour
+{
+    public GameObject futurePicture;
+    public GameObject currentPicture;
+    public GameObject oldPicture;
+
+    public void ChangePictureForward()
+    {
+        if (oldPicture != null)
+            oldPicture.SetActive(false);
+        currentPicture.SetActive(false);
+        futurePicture.SetActive(true);
+    }
+    public void ChangePictureBackward()
+    {
+        if (oldPicture != null)
+            oldPicture.SetActive(true);
+        else
+            Debug.Log("Can't change to a nonexistant picture");
+        currentPicture.SetActive(false);
+        futurePicture.SetActive(false);
+    }
+    private void Start()
+    {
+        Debug.Log("Hello World");
+    }
+
+    public void ChangeToScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
