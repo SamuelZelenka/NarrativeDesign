7ef19beaa79e06547d05f3deea143b6bb8aef2a3
peanutpowah <samuel.zelenka@hotmail.com>
2019-11-26 12:07:29 +0100

    KeypadScript + Mainscene update

---
+++ Sp18NarrativeDesign/Assets/Scripts/Progression/ObjectiveKeypad.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectiveKeypad : Interactible
+{
+    [SerializeField] DoorScript door;
+    [SerializeField] Material lightMaterial;
+
+    public override void Start()
+    {
+        lightMaterial.color = Color.red;
+    }
+
+    public override void Interact()
+    {
+        base.Interact();
+        if (!door.locked)
+        {
+            door.ToggleDoorOpen();
+            lightMaterial.color = Color.green;
+        }
+        else
+        {
+            lightMaterial.color = Color.red;
+        }
+
+    }
+
+}
