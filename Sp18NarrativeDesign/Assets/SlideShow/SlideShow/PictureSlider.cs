0f7e5421faa6bb1419eb4154f5f9dc744f12ad19
Juice-f <47383418+Juice-f@users.noreply.github.com>
2019-11-28 16:01:09 +0100

    öosefuwgscb

---
+++ Sp18NarrativeDesign/Assets/SlideShow/SlideShow/PictureSlider.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PictureSlider : MonoBehaviour
+{
+    public List<GameObject> futurePicture;
+    int pictureNumber = 0;
+
+    public void ForwardChange0()
+    {
+        pictureNumber = 0;
+        ChangePictureForward();
+    }
+    public void ForwardChange1()
+    {
+        pictureNumber = 1;
+        ChangePictureForward();
+    }
+    public void ForwardChange2()
+    {
+        pictureNumber = 2;
+        ChangePictureForward();
+    }
+    void ChangePictureForward()
+    {
+        gameObject.SetActive(false);
+        futurePicture[pictureNumber].SetActive(true);
+    }
+    public void ChangePictureBackward()
+    {
+            gameObject.SetActive(true);
+            Debug.Log("Can't change to a nonexistant picture");
+        futurePicture[pictureNumber].SetActive(false);
+    }
+    public void ChangeToScene()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
