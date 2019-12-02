929e1b9ccffc3921fb6270713285ac61e478d84a
peanutpowah <samuel.zelenka@hotmail.com>
2019-12-02 11:16:13 +0100

    push

--- Sp18NarrativeDesign/Assets/Scripts/Menu/Menu.cs
+++ Sp18NarrativeDesign/Assets/Scripts/Menu/Menu.cs
@@ -79,6 +79,12 @@ public class Menu : MonoBehaviour
     public void ExitGame(){
         Application.Quit();
     }
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1;
+    }
+
 
     public void PauseMenu()
     {
@@ -127,7 +133,7 @@ public class Menu : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (gameOverScreen.activeSelf == false)
+            if (gameOverScreen != null && gameOverScreen.activeSelf == false)
             {
 
                 if (optionsMenu.activeSelf)
