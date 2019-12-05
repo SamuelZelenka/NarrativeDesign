71a258a21bdf23cffadbbb256f9deb04e5e8ea42
Mohamed <mohamed.longis.hassan@gmail.com>
2019-12-05 12:36:28 +0100

    Light
    Light

--- Sp18NarrativeDesign/Assets/Scripts/Progression/ObjectiveKeypad.cs
+++ Sp18NarrativeDesign/Assets/Scripts/Progression/ObjectiveKeypad.cs
@@ -6,17 +6,21 @@ public class ObjectiveKeypad : Interactible
 {
     [SerializeField] DoorScript door;
     [SerializeField] Renderer lightRenderer;
-
+    [SerializeField] Light lt; // Mohamed
     public override void Start()
     {
+       
+
         base.Start();
         if (!door.locked)
         {
             lightRenderer.material.color = Color.green;
+            lt = GetComponent<Light>(); // Mohamed
         }
         else
         {
             lightRenderer.material.color = Color.red;
+            lt = GetComponent<Light>(); // mohamed 
         }
     }
 
@@ -27,12 +31,28 @@ public class ObjectiveKeypad : Interactible
         {
             door.ToggleDoorOpen();
             lightRenderer.material.color = Color.green;
+            lt = GetComponent<Light>();
         }
         else
         {
             lightRenderer.material.color = Color.red;
+            lt = GetComponent<Light>(); // Mohamed
         }
 
     }
 
+    //public void ToggleLight()
+    //{
+
+    //    if (!door.locked)
+    //    {
+    //        lightRenderer.material.color = Color.green;
+    //    }
+    //    else
+    //    {
+    //        lightRenderer.material.color = Color.red;
+    //    }
+    //}
 }
+
+  
