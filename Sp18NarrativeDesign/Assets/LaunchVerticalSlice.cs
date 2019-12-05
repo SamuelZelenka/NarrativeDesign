e144ce98308b0910f6934891d2e68c70daf5bf93
peanutpowah <samuel.zelenka@hotmail.com>
2019-12-05 16:28:27 +0100

    NarrativeDont fuck this up pliz

---
+++ Sp18NarrativeDesign/Assets/LaunchVerticalSlice.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LaunchVerticalSlice : MonoBehaviour
+{
+    [SerializeField]string newSceneName;
+    private void Awake()
+    {
+        SceneManager.LoadScene(newSceneName);
+    }
+}
